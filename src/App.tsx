8af3149109c81f606e17cfc8267d49ae80381104
Komefumi <komefumi@protonmail.ch>
2021-04-11 11:40:40 +0530

    Cleaning up and making use of the components in the main App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,18 @@
 import Navbar from 'components/Navbar';
 import Sidebar from 'components/Sidebar';
+import ContentArea from 'components/ContentArea';
 
 import main from 'styling/main.module.sass';
-
-// import logo from './logo.svg';
-// import './App.css';
+const { 'sidebar-and-content-wrapper': sidebarAndContentWrapper } = main;
 
 function App() {
   return (
     <div className={main.app}>
       <Navbar />
-      <Sidebar />
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={sidebarAndContentWrapper}>
+        <Sidebar />
+        <ContentArea />
+      </div>
     </div>
   );
 }
