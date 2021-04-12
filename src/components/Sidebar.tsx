ce6556b7d2fc12121bd9d6550639521c81ec9112
Komefumi <komefumi@protonmail.ch>
2021-04-12 19:22:26 +0530

    Adjust sidebar and add icons, using react-icons

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,3 +1,8 @@
+import { FaHome } from 'react-icons/fa';
+import { MdPhotoLibrary } from 'react-icons/md';
+import { MdVideoLibrary } from 'react-icons/md';
+import { FaUserCircle } from 'react-icons/fa';
+
 import sidebarStyles from 'styling/sidebar.module.sass';
 
 const {
@@ -7,14 +12,15 @@ const {
 } = sidebarStyles;
 
 export default function Sidebar() {
+  const size = "1.4em"
   return (
     <div className={containment}>
       <nav className={sidebarNav}>
         <ul className={listItems}>
-          <li>Item A</li>
-          <li>Item B</li>
-          <li>Item C</li>
-          <li>Item D</li>
+          <li><FaHome size={size} /></li>
+          <li><MdPhotoLibrary size={size} /></li>
+          <li><MdVideoLibrary size={size} /></li>
+          <li><FaUserCircle size={size} /></li>
         </ul>
       </nav>
     </div>
