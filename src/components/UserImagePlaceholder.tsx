93639ba25b898b7bd3b67c09db2abe6b99ac52f9
Komefumi <komefumi@protonmail.ch>
2021-04-13 19:08:27 +0530

    Truly replace the previous user profile image placeholder

--- src/components/UserImagePlaceholder.tsx
+++ src/components/UserImagePlaceholder.tsx
@@ -1,6 +1,6 @@
 import UserImagePlaceholderProps from 'types/UserImagePlaceholderProps';
 
-import PlaceholderUserImageSrc from 'assets/g843.png';
+import PlaceholderUserImageSrc from 'assets/g844.png';
 
 export default function UserImagePlaceholder({ size }: UserImagePlaceholderProps) {
   return (<img width={size} height={size} src={PlaceholderUserImageSrc} alt="User Placeholder" />);
