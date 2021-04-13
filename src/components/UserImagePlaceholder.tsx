18d3bf265df9a578656d9a0a7ff209c5eae0f6df
Komefumi <komefumi@protonmail.ch>
2021-04-13 16:19:28 +0530

    Remove unnecessary user image placeholder styling

--- src/components/UserImagePlaceholder.tsx
+++ src/components/UserImagePlaceholder.tsx
@@ -2,8 +2,6 @@ import UserImagePlaceholderProps from 'types/UserImagePlaceholderProps';
 
 import PlaceholderUserImageSrc from 'assets/g843.png';
 
-import styles from 'styling/user-image-placeholder.module.sass';
-
 export default function UserImagePlaceholder({ size }: UserImagePlaceholderProps) {
-  return (<img className={styles.img} width={size} height={size} src={PlaceholderUserImageSrc} alt="User Placeholder" />);
+  return (<img width={size} height={size} src={PlaceholderUserImageSrc} alt="User Placeholder" />);
 }
